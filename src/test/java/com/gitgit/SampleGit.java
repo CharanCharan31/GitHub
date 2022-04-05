aadcb26ffd806ecbd877aefa517ab2b8f22895bd
SakthiSri15052001 <sakthisrikeerthana@gmail.com>
2022-04-05 20:45:35 +0530

    sakthi commited

--- src/test/java/com/gitgit/SampleGit.java
+++ src/test/java/com/gitgit/SampleGit.java
@@ -15,6 +15,18 @@ public class SampleGit {
 		System.out.println("2");
 
 	}
+	public void method3() {
+		System.out.println("3");
+
+	}
+	public void method4() {
+		System.out.println("4");
+
+	}
+	public void method5() {
+		System.out.println("5");
+
+	}
 	
 	
 		
