8027d77c31e55250e1ba773ed87f87e02e918f84
GITHUB <charankalaiarasu@gmail.com>
2022-04-05 18:45:44 +0530

    AddCommit

---
+++ src/test/java/com/gitgit/SampleGit.java
@@ -0,0 +1,27 @@
+package com.gitgit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+public class SampleGit {
+	public void method1() {
+		System.out.println("1");
+
+	}
+	public void method2() {
+		System.out.println("2");
+
+	}
+	
+	
+		
+	
+		
+		
+		
+	}
+		
+
